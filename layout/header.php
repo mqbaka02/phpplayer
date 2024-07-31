17e0122e45dc16713ca5791bfb0bbe508df8201c
mqbaka <mqbaka@gmail.com>
2024-07-31 13:31:08 +0300

    favicon and stuff

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>phpPlayer</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico?v=1">
 </head>
 <body>
