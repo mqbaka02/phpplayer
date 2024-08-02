67d369cf015b4da6ed4c512d7359957571de43f2
mqbaka <mqbaka@gmail.com>
2024-08-02 10:48:08 +0300

    basic custom scrollbar for the directory explorer.

--- public/scripts/scrollbar.js
+++ public/scripts/scrollbar.js
@@ -1,4 +1,3 @@
-// console.log("hello scrbr");
 var scrollbarheight= 100;
 var maxScroll_value= 0;
 
@@ -14,20 +13,26 @@ if(!isOverflown(scrollable_el)){
     // console.log('overflown');
 } else {
     scrollbar.classList.remove("invisible");
-    // scrollbarheight= (scrollable_el.clientHeight / scrollable_el.scrollHeight) * 100;
     scrollbarheight= (scrollable_el.clientHeight * scrollable_el.clientHeight) / scrollable_el.scrollHeight;
     scrollbarheight= scrollbarheight.toFixed(2);
-    console.log(scrollbarheight);
+    // console.log(scrollbarheight);
     scrollable_el.style.setProperty("--scrb-hgt", scrollbarheight + "px");
     maxScroll_value= scrollable_el.scrollHeight - scrollable_el.clientHeight;
 }
 
 scrollable_el.addEventListener('scroll', event => {
     event.stopPropagation();
+    var scrollable_el_instance= event.target;
     // console.log("scrolling");
     // console.log(event.target.scrollTop);
-    var scrollbarPos= (event.target.scrollTop / maxScroll_value) * 100 * (scrollable_el.scrollHeight / scrollable_el.clientHeight);
-    scrollbarPos= scrollbarPos.toFixed(2);
-    console.log(scrollbarPos);
-    event.target.style.setProperty('--scrb-top', scrollbarPos + "%");
+    var possible_scroll= scrollable_el_instance.scrollHeight - scrollable_el_instance.clientHeight;
+    var possible_bar_scroll= scrollable_el_instance.scrollHeight - scrollbarheight;
+    var bar_top= scrollable_el_instance.scrollTop * possible_bar_scroll / possible_scroll;
+    scrollable_el_instance.style.setProperty("--scrb-top", bar_top + "px");
+    /**
+     * scroll_distance= scrollheight - clientheight
+     * bar_scroll_dist= scrollheight - barheight
+     * $scrolltop / scroll_dist = $bar_top / barscrolldist;
+     * $bar_top= $scrolltop * barscrolldist / scroll_distance;
+     */
 });
